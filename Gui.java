4aa44e9f1b28a8772a142b68159c2cb48e18b770
Gizm02 <gizm0@gmx.net>
2013-08-17 11:30:34 +0200

    first commit

--- Gui.java
+++ Gui.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
-
+import javax.swing.KeyStroke;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.Desktop;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JTabbedPane;
+import javax.swing.JMenuItem;
 import java.io.*;
 import java.net.URI; //Use Uniform Resource Identifier-class for browsing-option in context "help"
 import javax.media.jai.*;
@@ -16,7 +17,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  * @author Gizm02
  * @version 18.09.2012
  */
-public class Gui extends Frame
+public class Gui extends JFrame
 {
 	private JFileChooser jfc;
     JPanel j;
@@ -42,15 +43,16 @@ public class Gui extends Frame
     
     public void create() {
         // Create general content, the border
-        MenuBar m = new MenuBar();
-        Menu sub = new Menu("Program");
-        MenuItem quit = new MenuItem("Quit");
+        JMenuBar m = new JMenuBar();
+        JMenu sub = new JMenu("Program");
+        JMenuItem quit = new JMenuItem("Quit");
         // Klasse System, void exit(int status), Wert groesser 0 bedeutet Abbruch durch Fehler,
         // = 0 sauberes Beenden
         quit.addActionListener(new ActionListener() {
                                                     public void actionPerformed(ActionEvent e) {
                                                         System.exit(0);
                                                     }});
+        quit.setAccelerator(KeyStroke.getKeyStroke('Q',InputEvent.CTRL_MASK));
         // To open several images simultaneously tabs are used
         final JTabbedPane tp = new JTabbedPane();
         // Add a possibility to open files via the Gui, this is realized through
@@ -60,7 +62,7 @@ public class Gui extends Frame
         // String getDirectory()      returns the name of the directory the chosen file is in
         // String getFile()           returns the name of the chosen file, not an absolute path
         // void show()                starts the dialog
-        MenuItem open = new MenuItem("Open");
+        JMenuItem open = new JMenuItem("Open");
         open.addActionListener(new ActionListener() {
                 @SuppressWarnings("deprecation")
 				public void actionPerformed(ActionEvent e) {
@@ -77,7 +79,8 @@ public class Gui extends Frame
                     tp.addTab(f.toString(), p);
                     resize(tp.getWidth(),tp.getHeight());
                     }});
-        MenuItem store = new MenuItem("Store");
+        open.setAccelerator(KeyStroke.getKeyStroke('O',InputEvent.CTRL_MASK));
+        JMenuItem store = new JMenuItem("Store");
         store.addActionListener(new ActionListener() {
         		public void actionPerformed(ActionEvent e) {
         			File f = new File("");
@@ -91,39 +94,40 @@ public class Gui extends Frame
         				f = jfc.getSelectedFile();
         			p.store(f.toString());
        	} } );
+        store.setAccelerator(KeyStroke.getKeyStroke('S',InputEvent.CTRL_MASK));
         add(tp);
         // Add the menu items to the menu bar
         sub.add(store);
         sub.add(open);
         sub.add(quit);
         m.add(sub);
-        Menu edit = new Menu("Edit");
-        MenuItem e = new MenuItem("Edit color");
+        JMenu edit = new JMenu("Edit");
+        JMenuItem e = new JMenuItem("Edit color");
         e.addActionListener( new ActionListener() {
-                                                public void actionPerformed(ActionEvent e) {
-                                                        p.addConst2();
-                                                }
-                                                });
-       MenuItem i = new MenuItem("Invert color");
+                        public void actionPerformed(ActionEvent e) {
+                             p.addConst2();
+                          }
+                        });
+       JMenuItem i = new JMenuItem("Invert color");
        i.addActionListener( new ActionListener() {
-                                                public void actionPerformed(ActionEvent e) {
-                                                    p.invert2();
-                                                }});
-       MenuItem rotate= new MenuItem("Rotate Image");
+                        public void actionPerformed(ActionEvent e) {
+                            p.invert2();
+                        }});
+       JMenuItem rotate= new JMenuItem("Rotate Image");
        rotate.addActionListener(new ActionListener() {
     	   public void actionPerformed(ActionEvent e) {
     		   p.rotate();
     	   }
        });
-       MenuItem brows = new MenuItem("Help");
+       JMenuItem brows = new JMenuItem("Help");
        brows.addActionListener(new ActionListener() { //Open Webbrowser:
-    	   										public void actionPerformed(ActionEvent e) {
-    	   											try {
-    	   												Desktop.getDesktop().browse(new URI("http://www.oracle.com/"));
-    	   											}
-    	   											catch(Exception e2) {
-    	   											};
-    	   										}
+    	   				public void actionPerformed(ActionEvent e) {
+    	   					try {
+    	   						Desktop.getDesktop().browse(new URI("http://www.oracle.com/"));
+    	   					}
+    	   					catch(Exception e2) {
+    	   					};
+    	   	}
        });
        edit.add(brows);
        edit.add(i);
@@ -131,7 +135,7 @@ public class Gui extends Frame
        edit.add(rotate);
        m.add(edit);
        // Add the Menubar
-       setMenuBar(m);
+       setJMenuBar(m);
        //Choose an image for an icon to be displayed in the upper left corner of the JFrame:
        /*try {
        setIconImage(ImageIO.read(new File("biohazard004.jpg")));
